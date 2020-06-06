86945d411ada3b7253ca11e5899d94e18917dd40
Ayesha Khalid <ayeshakhalidsh@gmail.com>
2020-06-06 14:01:13 -0400

    Contoller

---
+++ EqualsControllerCC.java
@@ -0,0 +1,35 @@
+//Class Name:EqualsControllerCC   class
+//Programmed by: Ayesha Khalid
+//Last Modified: December 29th 2018
+//Description: sets the values from the text fields as the variables and calls the solution method in the model to solve for an answer
+
+//imports
+import javax.swing.*;
+import java.awt.event.*;
+
+//class 
+public class EqualsControllerCC extends Object implements ActionListener
+{
+     //instance variables of model and textfield 
+     private ConversionClass ccModel;         //instance of forces and dynamics model class
+     private JTextField baseValue;       //instance of forces and dynamics base value text field
+     
+     //constructor for EqualsControllerCC has the current model object and all CC text fields 
+     public EqualsControllerCC(ConversionClass aModel, JTextField bV)
+     {
+          //sets the values of instance variables to the arguments provided in the constructor
+          this.ccModel = aModel;
+          this.baseValue = bV;
+     }//end of constructor
+     
+     public void actionPerformed(ActionEvent e)
+     {
+//takes user input and sets it in model class
+          double userInput;
+          userInput=Double.parseDouble(this.baseValue.getText());
+          this.ccModel.setConverter(userInput);
+//calls the solution method in the CC model to solve for an answer
+          this.ccModel.solution();
+     }//end of actionPerformed
+}//end of EqualsControllerCC class
+
